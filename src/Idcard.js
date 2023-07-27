c3708636b928fcf7670195a8a5a14f5b150ea9bc
Brendan Murphy <88051756+bpmurphy8@users.noreply.github.com>
2023-07-27 16:26:32 -0700

    Update Idcard.js

--- src/Idcard.js
+++ src/Idcard.js
@@ -1,14 +1,14 @@
 const Idcard = (props) => {
     return ( 
         <div>
-            <div class="card w-25 bg-info text-white float-start mx-2 my-2">
-                <div class="card-header bg-primary"><h2>Name: {props.name}</h2></div>
-                <div class="card-body">
-                    <h4 class="card-title"><h3>State: {props.state}</h3></h4>
-                    <p class="card-text">Age: {props.age}</p>
+            <div className="card w-25 bg-info text-white float-start mx-2 my-2">
+                <div className="card-header bg-primary"><h2>Name: {props.name}</h2></div>
+                <div className="card-body">
+                    <h4 className="card-title"><h3>State: {props.state}</h3></h4>
+                    <p className="card-text">Age: {props.age}</p>
                 </div>
-                <div class="card-footer bg-primary">
-                    <button class="btn btn-info text-white">More...</button>
+                <div className="card-footer bg-primary">
+                    <button className="btn btn-info text-white">More...</button>
                 </div>
             </div>
         </div>
